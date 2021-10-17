288d03b1288729d846b3611286bb0708ff3eb8d0
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-10-17 09:44:08 +0530

    5

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -2,22 +2,15 @@ package module5
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 )
 
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
-
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
 		fmt.Println("something went wrong")
 	}
 
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(resp.Body)
+	defer resp.Body.Close()
 }
